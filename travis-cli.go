43b60b0f4697eb72ec34626324ebdf9be0d3599a
Lennard Wolf <lennardwolf@live.de>
2016-04-08 11:50:24 +0200

    add login.go

--- travis-cli.go
+++ travis-cli.go
@@ -1,18 +1,31 @@
 package main
 
 import (
-  "os"
-  "github.com/codegangsta/cli"
+	"os"
+
+	"github.com/codegangsta/cli"
 )
 
 func main() {
-  app := cli.NewApp()
-  app.Name = "cli"
-  app.Usage = "a travis-ci command line client"
-  app.Action = func(c *cli.Context) {
-    println("hello world!")
-  }
+	app := cli.NewApp()
+	app.EnableBashCompletion = true
+	app.Name = "cli"
+	app.Usage = "a travis-ci command line client"
+	app.Action = func(c *cli.Context) {
+		//github := github.NewClient(nil)
+		println("main!")
 
-  app.Run(os.Args)
+	}
+	app.Commands = []cli.Command{
+		{
+			Name:    "login",
+			Aliases: []string{"a"},
+			Usage:   "add a task to the list",
+			Action: func(c *cli.Context) {
+				login()
+			},
+		},
+	}
+
+	app.Run(os.Args)
 }
-
